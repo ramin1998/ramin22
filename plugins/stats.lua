10d88f53e72594b709c7f5c46e5533b358d12b02
£FAILEBABURIANLOPSMMBQTFS <kashme.shab@gmail.com>
2015-12-22 17:42:47 +0330

    Update stats.lua

--- plugins/stats.lua
+++ plugins/stats.lua
@@ -138,11 +138,11 @@ local function run(msg, matches)
 end
 return {
   patterns = {
-    "^[!/]([Ss]tats)$",
-    "^[!/]([Ss]tatslist)$",
-    "^[!/]([Ss]tats) (group) (%d+)",
-    "^[!/]([Ss]tats) (test)",-- Put everything you like :)
-		"^[!/]([tt]est)"-- Put everything you like :)
+    "^([Ss]tats)$",
+    "^([Ss]tatslist)$",
+    "^([Ss]tats) (group) (%d+)",
+    "^([Ss]tats) (test)",-- Put everything you like :)
+		"^([tt]est)"-- Put everything you like :)
     }, 
   run = run
 }
